0860e8666f770079eb2b6eaf86e5e5d0a8c21c94
Eduardo Tavares <eduardo.tavares@cortex-intelligence.com>
2016-12-06 15:03:41 -0200

    Ajustes

--- runge-kutta.py
+++ runge-kutta.py
@@ -1,53 +1,38 @@
+from tabulate import tabulate
 import time
+import csv
 
-def rk4(x, y, n):
-    def f(x, y):
-        return x + y + 2
+algs = {
+    'euler': euler,
+    'runge-kutta-2': rk2,
+    'runge-kutta-4': rk4
+}
 
-    x = 0
-    y = 2
+def func(x,y):
+    pass
+
+def euler():
+    pass
+
+def rk4(f, x, y, h):
     h = 0.1
 
-    while(True):
-        k1 = f(x, y)
-        k2 = f(x + (h/2), y + h/2 * k1)
-        k3 = f(x + (h/2), y + h/2 * k2)
-        k4 = f(x + h, y + h * k3)
+    k1 = f(x, y)
+    k2 = f(x + (h/2), y + h/2 * k1)
+    k3 = f(x + (h/2), y + h/2 * k2)
+    k4 = f(x + h, y + h * k3)
 
-        print('x', x, sep=' = ')
-        print('y', y, sep=' = ')
-        print('k1', k1, sep=' = ')
-        print('k2', k2, sep=' = ')
-        print('k3', k3, sep=' = ')
-        print('k4', k4, sep=' = ')
-        print()
+    print()
 
-        x = x + h
-        y = y + (h/6) * (k1 + (2*k2) + (2*k3) + k4)
+    x = x + h
+    y = y + (h/6) * (k1 + (2*k2) + (2*k3) + k4)
 
-        time.sleep(1)
-        i += 1
+def rk2(f, x, y, h)):
 
-def rk2(x, y, n):
-    def f(x, y):
-        return x + y + 2
+    k1 = f(x, y)
+    k2 = f(x, y + h*k1)
 
-    x = 0
-    y = 2
-    h = 0.1
+    print()
 
-    while(True):
-        k1 = f(x, y)
-        k2 = f(x, y + h*k1)
-
-        print('x', x, sep=' = ')
-        print('y', y, sep=' = ')
-        print('k1', k1, sep=' = ')
-        print('k2', k2, sep=' = ')
-        print()
-
-        x = x + h
-        y = y + h/2 * (k1 + k2)
-
-        time.sleep(1)
-        i += 1
+    x = x + h
+    y = y + h/2 * (k1 + k2)
