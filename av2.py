2d9e69c382a278c7ca4cf59e75fa0c194caf5126
Eduardo Tavares <eduardo.tavares@cortex-intelligence.com>
2016-12-07 15:56:26 -0200

    Ajuste no runge-Kutta de 2ª ordem

--- av2.py
+++ av2.py
@@ -19,18 +19,28 @@ def euler(f, x, xn, y, h, err):
     print_table(header, rows)
 
 def rungekutta2(f, x, xn, y, h, err):
-    header = ['X', 'Y', 'K1', 'K2']
+    header = ['X', 'Y (Heun)', 'Y (Ponto Médio)', 'Y (Ralston)',
+        'K1', 'K2 (Heun)', 'K2 (Ponto Médio)', 'K2 (Ralston)']
     rows = list()
+
+    yh = y
+    ym = y
+    yr = y
 
     while round(xn, err) >= round(x, err):
         k1 = f(x, y)
-        k2 = f(x, y + h*k1)
+        k2h = f(x + h, y + k1*h)
+        k2m = f(x + h/2, y + k1*(h/2))
+        k2r = f(x + 3*h/4, y + k1*(3*h/4))
 
-        rows.append([round(x, err), round(y, err),
-            round(k1, err), round(k2, err)])
+        rows.append([round(x, err), round(yh, err), round(ym, err),
+            round(yr, err), round(k1, err), round(k2h, err),
+            round(k2m, err), round(k2r, err)])
 
         x = x + h
-        y = y + h/2 * (k1 + k2)
+        yh = yh + (k1/2 + k2h/2) * h
+        ym = ym + k2m * h
+        yr = yr + (k1/3 + 2*k2r/3) * h
 
     print_table(header, rows)
 
@@ -64,11 +74,6 @@ if __name__ == '__main__' and len(sys.argv) == 3:
     with open(sys.argv[2], 'r') as f:
         equation = f.readline()
         values = f.readline().split(',')
-        err = f.readline()
-        if err:
-            err = int(err)
-        else:
-            err = 4
 
         for i, value in enumerate(values):
             values[i] = float(value)
@@ -76,9 +81,9 @@ if __name__ == '__main__' and len(sys.argv) == 3:
     def f(x, y=None):
         return eval(equation)
 
-    alg_func(f, values[0], values[1], values[2], values[3], err)
+    alg_func(f, values[0], values[1], values[2], values[3], 10)
 else:
-    print('''
+    print('''\
 Como usar:
 python av2.py [sigla do algoritmo] [nome do arquivo de texto de entrada]
 
@@ -93,7 +98,6 @@ Algorítmos suportados e suas siglas:
 Os arquivos de texto devem ter a seguinte estrutura:
 Linha 1: Equação (com sintaxe do python)
 Linha 2: X inicial, X final, Y inicial e H (separados por virgula)
-Linha 3: Quantidade máxima de casas decimais (padrão: 4)
 (H = o intervalo entre cada valor de X)
 
 Ex. input.txt:
